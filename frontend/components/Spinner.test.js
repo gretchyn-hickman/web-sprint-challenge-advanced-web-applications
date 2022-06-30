d4cbcddb5d8a5d44cf306d8fe277225802a3437c
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-29 23:30:52 -0600

    oops one more, now I'm done

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,3 @@
-// Import the Spinner component into this file and test
-// that it renders what it should for the different props it can take.
 import Spinner from "./Spinner";
 import React from "react";
 import {
